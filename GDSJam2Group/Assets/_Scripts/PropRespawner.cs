d481357de74e829054040b960277832692900acf
Akabird <worstonthenet@gmail.com>
2023-09-09 20:37:57 +1200

    Respawning props (real) (not clickbait)
    didn't realise that there weren't obstacles to respawn and i wanted to do pickups seperate to other props. oh well. here's the respawning scripts for props. changes the machine scripts as well. :)

--- GDSJam2Group/Assets/_Scripts/PropRespawner.cs
+++ GDSJam2Group/Assets/_Scripts/PropRespawner.cs
@@ -18,12 +18,7 @@ public class PropRespawner : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        
-    }
-
-    public void RespawnProp(){
+    public void LaunchProp(){
         rb.velocity = Vector2.zero;
         transform.position = respawnLocation.position;
         transform.rotation = respawnLocation.rotation;
